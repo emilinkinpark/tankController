1a215f7a4603add067dfb3cf06fc78f4137abaa4
emilinkinpark <emilinkinpark@gmail.com>
2020-03-19 14:18:12 +0600

    BME680 implemented

---
+++ include/bme680.cpp
@@ -0,0 +1,62 @@
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME680.h>
+
+Adafruit_BME680 bme; // I2C
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+
+void bmeInit()
+{
+
+  while (!Serial);
+  Serial.println(F("BME680 test"));
+ 
+  if (!bme.begin(0x77)) 
+  {
+    Serial.println("Could not find a valid BME680 sensor, check wiring!");
+    while (1);
+  }
+ 
+  // Set up oversampling and filter initialization
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320*C for 150 ms
+}
+
+
+void bmeRun()
+{
+if (! bme.performReading()) 
+  {
+    Serial.println("Failed to perform reading :(");
+    
+  }
+  Serial.print("Temperature = ");
+  Serial.print(bme.temperature);
+  Serial.println(" *C");
+ 
+  Serial.print("Pressure = ");
+  Serial.print(bme.pressure / 100.0);
+  Serial.println(" hPa");
+ 
+  Serial.print("Humidity = ");
+  Serial.print(bme.humidity);
+  Serial.println(" %");
+ 
+  Serial.print("Gas = ");
+  Serial.print(bme.gas_resistance / 1000.0);
+  Serial.println(" KOhms");
+ 
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+ 
+  Serial.println();
+  delay(2000);
+  }
--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,47 @@
 *   The MODBUSRTU communication protocol is implemented according to Modbus_over_serial_line_V1_02.pdf
 *   Only MODBUS Holding Register (0x03) is implemented.
 */
+
+
+
+
 #include "modbus.h"
 #include "modbus.cpp"
 #include "conversions.cpp"
 
+#include "bme680.cpp"
+
+//Serial Pins Definition
+
+#define UART1_RX  4
+#define UART1_TX  2
+#define UART2_RX  16
+#define UART2_TX  17
+
+
+// TCS3200 Color Sensor 
+
+// TCS230 or TCS3200 pins wiring to Arduino
+#define S0 32
+#define S1 33
+#define S2 25
+#define S3 26
+#define sensorOut 35
+
+// Stores frequency read by the photodiodes
+int redFrequency = 0;
+int greenFrequency = 0;
+int blueFrequency = 0;
+
+
+
+
+
+// DO sensor ID
 #define O2_slaveID 0x0E
 #define O2_slaveID_DEC 14
 
+// Global Variables for Temperature and DO mg/L for Optical DO Sensor
 uint16_t Temp_Send = 0;
 uint16_t DOmgl_Send = 0;
 uint16_t temp_transmit = 0;
@@ -28,23 +62,87 @@ int incomingData[7];
 void Core0code(void *pvParameters) //All other sensor and MQTT interface
 {
   for (;;)
-  {
+ {
+
+   bmeRun();          // Running bme680 codes
+//   //TCS3200 Code Start 
+//   // Setting RED (R) filtered photodiodes to be read
+//   digitalWrite(S2,LOW);
+//   digitalWrite(S3,LOW);
+  
+//   // Reading the output frequency
+//   redFrequency = pulseIn(sensorOut, LOW);
+  
+//    // Printing the RED (R) value
+//   Serial.print("R = ");
+//   Serial.print(redFrequency);
+//   delay(100);
+  
+//   // Setting GREEN (G) filtered photodiodes to be read
+//   digitalWrite(S2,HIGH);
+//   digitalWrite(S3,HIGH);
+  
+//   // Reading the output frequency
+//   greenFrequency = pulseIn(sensorOut, LOW);
+  
+//   // Printing the GREEN (G) value  
+//   Serial.print(" G = ");
+//   Serial.print(greenFrequency);
+//   delay(100);
+ 
+//   // Setting BLUE (B) filtered photodiodes to be read
+//   digitalWrite(S2,LOW);
+//   digitalWrite(S3,HIGH);
+  
+//   // Reading the output frequency
+//   blueFrequency = pulseIn(sensorOut, LOW);
+  
+//   // Printing the BLUE (B) value 
+//   Serial.print(" B = ");
+//   Serial.println(blueFrequency);
+//   delay(100);
+// // TCS3200 code end
+
   }
 }
 
 void setup()
 {
 
-  xTaskCreatePinnedToCore(Core0code, "Task0", 10000, NULL, 1, NULL, 0);
+  xTaskCreatePinnedToCore(Core0code, "Task0", 10000, NULL, 1, NULL, 0);   // Handle to access core 0
 
   Serial.begin(9600); //TXD0 - used as serial decorder
 
-  Serial1.begin(9600, SERIAL_8N1, 4, 2);
-  /* UART1 Rx Pin = GPIO 4 and TX Pin = GPIO 2  
-  Caution: Remove Pins before uploading firmware!!!!! */
+  Serial1.begin(9600, SERIAL_8N1, UART1_RX, UART1_TX); 
+  //Caution: Remove Pins before uploading firmware!!!!! // Shared with Flash
 
-  Serial2.begin(9600); /*  UART2 Rx Pin = GPIO 16 and TX Pin = GPIO 17 */
+  Serial2.begin(9600,SERIAL_8N1,UART2_RX,UART2_TX); 
+
+
+  // // TCS3200 Color Sensor Setup
+  // // Setting the outputs
+  // pinMode(S0, OUTPUT);
+  // pinMode(S1, OUTPUT);
+  // pinMode(S2, OUTPUT);
+  // pinMode(S3, OUTPUT);
+  
+  // // Setting the sensorOut as an input
+  // pinMode(sensorOut, INPUT);
+  
+  // // Setting frequency scaling to 20%
+  // digitalWrite(S0,HIGH);
+  // digitalWrite(S1,LOW);
+  // // TCS3200 Sensor Setup end
+
+  bmeInit();      // Initialising BME680 dependents
+ 
+
+
+
 }
+
+
+
 
 void loop() // All Modbus Operation
 {
